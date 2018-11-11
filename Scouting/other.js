92b5d8fdd428737b16975f8ac56147e5219de526
Takumi <takumiichihashi@gmail.com>
2018-11-10 21:39:34 -0800

    scouting is removed

--- Scouting/other.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("myBtn").onclick = function() {myFunction()};
-
-/* myFunction toggles between adding and removing the show class, which is used to hide and show the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
